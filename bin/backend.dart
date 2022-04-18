61d1168c5f42675ab4e9c54f5f3cb78453fcbd3b
xcaio <mrlose22@gmail.com>
2022-04-17 22:50:30 -0300

    Primeiro Commit

---
+++ bin/backend.dart
@@ -0,0 +1,16 @@
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+
+import 'server_handler.dart';
+
+void main() async{
+  var _server = ServeHandler();
+
+  //CRIANDO UM SERVIDOR BACKEND PARA GENTE TRABALHAR
+  final server = await shelf_io.serve(
+    _server.handler, 'localhost', 8080);
+
+  print("Nosso servidor foi iniciado http://localhost:8080");
+
+}
